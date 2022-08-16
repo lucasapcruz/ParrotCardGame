df6ed13c88abc955a11553279a189a5a054064bf
Lucas Cruz <lucas.apcruz@gmail.com>
2022-08-16 20:07:28 -0300

    Add card selection feature

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let numberOfcards = prompt("Com quantas cartas quer jogar?");
 
+const lsOfCards = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+];
+let lsOfSelectedCards;
+
 while((numberOfcards < 4) || (numberOfcards%2 !== 0)){
     numberOfcards = prompt("Número de cartas inválido. Com quantas cartas quer jogar?")
 }
 
+lsOfSelectedCards = lsOfCards.slice(0,(numberOfcards/2)-1)
+lsOfSelectedCards = lsOfSelectedCards.concat(lsOfSelectedCards)
+lsOfSelectedCards.sort(comparator)
 
-
+function comparator() { 
+	return Math.random() - 0.5; 
+}
