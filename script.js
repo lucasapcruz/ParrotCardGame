4f65181b00aa15465d5c5add8b58b67f2bc281b0
Lucas Cruz <lucas.apcruz@gmail.com>
2022-08-16 20:41:22 -0300

    Add card display

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ const lsOfCards = [
 let lsOfSelectedCards;
 
 while((numberOfcards < 4) || (numberOfcards%2 !== 0)){
-    numberOfcards = prompt("Número de cartas inválido. Com quantas cartas quer jogar?")
+    numberOfcards = prompt("Número de cartas inválido. Com quantas cartas quer jogar?");
 }
 
-lsOfSelectedCards = lsOfCards.slice(0,(numberOfcards/2)-1)
-lsOfSelectedCards = lsOfSelectedCards.concat(lsOfSelectedCards)
-lsOfSelectedCards.sort(comparator)
+lsOfSelectedCards = lsOfCards.slice(0,(numberOfcards/2));
+lsOfSelectedCards = lsOfSelectedCards.concat(lsOfSelectedCards);
+lsOfSelectedCards.sort(comparator);
+
+const deckElm = document.querySelector(".deck");
+
+for(let i=0; i < lsOfSelectedCards.length; i++){
+    deckElm.innerHTML = deckElm.innerHTML + `<div class="card face-down"><img src="img/front.png"></div>`;
+}
 
 function comparator() { 
 	return Math.random() - 0.5; 
