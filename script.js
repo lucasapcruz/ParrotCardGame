9fd48a085aa3523a7e7fca9d820c93a2866c6b9f
Lucas Cruz <lucas.apcruz@gmail.com>
2022-08-17 23:25:35 -0300

    Add game logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberOfcards = prompt("Com quantas cartas quer jogar?");
+let numberOfCards = prompt("Com quantas cartas quer jogar?");
 
 const lsOfCards = [
     "bobrossparrot",
@@ -11,20 +11,70 @@ const lsOfCards = [
 ];
 let lsOfSelectedCards;
 
-while((numberOfcards < 4) || (numberOfcards%2 !== 0)){
-    numberOfcards = prompt("Número de cartas inválido. Com quantas cartas quer jogar?");
+while((numberOfCards < 4) || (numberOfCards%2 !== 0)){
+    numberOfCards = prompt("Número de cartas inválido. Com quantas cartas quer jogar?");
 }
 
-lsOfSelectedCards = lsOfCards.slice(0,(numberOfcards/2));
+lsOfSelectedCards = lsOfCards.slice(0,(numberOfCards/2));
 lsOfSelectedCards = lsOfSelectedCards.concat(lsOfSelectedCards);
 lsOfSelectedCards.sort(comparator);
 
 const deckElm = document.querySelector(".deck");
 
 for(let i=0; i < lsOfSelectedCards.length; i++){
-    deckElm.innerHTML = deckElm.innerHTML + `<div class="card face-down"><img src="img/front.png"></div>`;
+    deckElm.innerHTML = deckElm.innerHTML +
+    `<div class="card" onclick="selectCard(this)">
+        <div class="face front-face"><img src="img/front.png"></div>
+        <div class="face back-face"><img src="img/${lsOfSelectedCards[i]}.gif"></div>
+     </div>`;
+
 }
+
+let numOfMoves = 0;
+let pairForComparison = [];
+const lsOfCardElements = document.querySelectorAll(".card");
+
 
 function comparator() { 
 	return Math.random() - 0.5; 
+}
+
+function selectCard(element){
+    if(!element.classList.contains("selected")){
+        if(pairForComparison.length<2){
+            element.classList.add("selected");
+            numOfMoves++
+            const iCard = Array.from(lsOfCardElements).indexOf(element);
+            pairForComparison.push(iCard);
+            console.log(pairForComparison)
+        }
+        if(pairForComparison.length == 2){
+            verifyMatchingCards(pairForComparison)
+        }
+    }
+    setTimeout('verifyWin()', 600);
+}
+
+function verifyMatchingCards(pair){
+    const card1 = lsOfSelectedCards[pair[0]];
+    const card2 = lsOfSelectedCards[pair[1]];
+    
+    if(card1 !== card2){
+        setTimeout(function(){
+                lsOfCardElements[pair[0]].classList.remove("selected");
+                lsOfCardElements[pair[1]].classList.remove("selected");
+            }, 1000);
+    }
+    
+    pairForComparison = [];
+
+}
+
+function verifyWin(){
+    
+    const matchedCards = document.querySelectorAll(".selected");
+    
+    if(matchedCards.length == numberOfCards){
+        alert(`Você ganhou em ${numOfMoves} jogadas!`)
+    }
 }
